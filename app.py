97c7137c6554c22391e7d7adcc95711b1c5384c6
Gael <gaelestgon@gmail.com>
2026-06-27 20:26:05 +0200

    refactor: extract tradingview widgets

--- app.py
+++ app.py
@@ -58,6 +58,7 @@ from modulos.market_widgets import (
     buscar_etf_yahoo,
     render_ticker_tape,
 )
+from modulos.tradingview_widgets import render_tradingview_widget, renderizar_grafico_tradingview
 from modulos.app_runtime import build_runtime_paths
 from modulos.module_loader import safe_call
 from modulos.utils import cargar_datos, calcular_score_buffett, analizar_sentimiento_noticias as analizar_sentimiento_noticias_utils
@@ -213,78 +214,6 @@ def obtener_tickers_filtrados():
 # ==========================================
 # WIDGET RADICAL 2: MOTOR TRADINGVIEW EN VIVO
 # ==========================================
-def render_tradingview_widget(ticker):
-    """Inyecta el terminal avanzado interactivo de TradingView mediante iframe"""
-    
-    # Algunos tickers de Yahoo Finance (ej. BRK-B) necesitan limpieza para TradingView
-    ticker_tv = ticker.replace("-", ".") 
-    
-    html_code = f"""
-    <!-- TradingView Widget BEGIN -->
-    <div class="tradingview-widget-container" style="height:100%;width:100%">
-      <div id="tradingview_terminal" style="height:calc(100% - 32px);width:100%"></div>
-      <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
-      <script type="text/javascript">
-      new TradingView.widget(
-      {{
-      "autosize": true,
-      "symbol": "{ticker_tv}",
-      "interval": "D",
-      "timezone": "exchange",
-      "theme": "dark",
-      "style": "1",
-      "locale": "es",
-      "enable_publishing": false,
-      "backgroundColor": "#0b1426",
-      "gridColor": "#1e3354",
-      "hide_top_toolbar": false,
-      "hide_legend": false,
-      "save_image": false,
-      "container_id": "tradingview_terminal",
-      "toolbar_bg": "#0b1426"
-    }}
-      );
-      </script>
-    </div>
-    <!-- TradingView Widget END -->
-    """
-    # Renderizamos el HTML incrustado con una altura de 600 píxeles
-    components.html(html_code, height=600)
-
-def renderizar_grafico_tradingview(ticker):
-    """Inyecta el widget avanzado y nativo de TradingView interactivo"""
-    codigo_html = f"""
-    <div class="tradingview-widget-container" style="height:100%;width:100%">
-      <div id="tv_chart_container" style="height:600px;width:100%"></div>
-      <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
-      <script type="text/javascript">
-      new TradingView.widget(
-      {{
-      "autosize": true,
-      "symbol": "{ticker}",
-      "interval": "D",
-      "timezone": "Etc/UTC",
-      "theme": "dark",
-      "style": "1",
-      "locale": "es",
-      "enable_publishing": false,
-      "backgroundColor": "#0b0e14",
-      "gridColor": "#1f293d",
-      "hide_top_toolbar": false,
-      "hide_legend": false,
-      "save_image": false,
-      "container_id": "tv_chart_container",
-      "toolbar_bg": "#131722",
-      "studies": [
-        "Volume@tv-basicstudies",
-        "MASimple@tv-basicstudies"
-      ]
-    }}
-      );
-      </script>
-    </div>
-    """
-    components.html(codigo_html, height=600)
 
 def obtener_valoracion_sectorial(ticker):
     """Aplica la regla de valoración relativa según el sector (Basado en el marco institucional)"""
